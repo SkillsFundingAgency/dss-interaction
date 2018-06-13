98fb9b65f5855036273b4816b8166b2877f39c29
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:50:47 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Interaction/PostInteractionHttpTrigger/PostInteractionHttpTrigger.cs
+++ NCS.DSS.Interaction/PostInteractionHttpTrigger/PostInteractionHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Interaction.PostInteractionHttpTrigger
     public static class PostInteractionHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Interaction))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Interaction C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var interaction = await req.Content.ReadAsAsync<Models.Interaction>();
+
+            var interactionService = new PostInteractionHttpTriggerService();
+            var interactionId = interactionService.Create(interaction);
+
+            return interactionId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Interaction record with Id of : " + interactionId)
+                };
         }
     }
 }
--- NCS.DSS.Interaction/PostInteractionHttpTrigger/PostInteractionHttpTriggerService.cs
+++ NCS.DSS.Interaction/PostInteractionHttpTrigger/PostInteractionHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Interaction.PostInteractionHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Interaction.PostInteractionHttpTrigger
 {
     public class PostInteractionHttpTriggerService
     {
+        public Guid? Create(Models.Interaction interaction)
+        {
+            if (interaction == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
