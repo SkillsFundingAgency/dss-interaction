bf6281e7a2ff892896f9c84cfcc57c60fd8c7c5a
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-09 16:15:40 +0100

    Add Unit Tests

---
+++ NCS.DSS.Interaction.Tests/GetInteractionHttpTriggerTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions;
+using Microsoft.Azure.WebJobs.Host;
+using NCS.DSS.Interaction.Cosmos.Helper;
+using NCS.DSS.Interaction.GetInteractionHttpTrigger.Service;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace NCS.DSS.Interaction.Tests
+{
+    [TestFixture]
+    public class GetInteractionHttpTriggerTest
+    {
+        private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+        private const string InValidId = "1111111-2222-3333-4444-555555555555";
+        private TraceWriter _log;
+        private HttpRequestMessage _request;
+        private IResourceHelper _resourceHelper;
+        private IGetInteractionHttpTriggerService _getInteractionHttpTriggerService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _request = new HttpRequestMessage()
+            {
+                Content = new StringContent(string.Empty),
+                RequestUri =
+                    new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/Interactions/")
+            };
+            _log = new TraceMonitor();
+            _resourceHelper = Substitute.For<IResourceHelper>();
+            _getInteractionHttpTriggerService = Substitute.For<IGetInteractionHttpTriggerService>();
+        }
+        [Test]
+        public async Task GetInteractionHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(InValidId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetInteractionHttpTrigger_ReturnsStatusCodeNoContent_WhenCustomerDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetInteractionHttpTrigger_ReturnsStatusCodeNoContent_WhenInteractionDoesNotExist()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+
+            _getInteractionHttpTriggerService.GetInteractionsAsync(Arg.Any<Guid>()).Returns(Task.FromResult<List<Models.Interaction>>(null).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetInteractionHttpTrigger_ReturnsStatusCodeOk_WhenInteractionExists()
+        {
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+
+            var listOfInteractiones = new List<Models.Interaction>();
+            _getInteractionHttpTriggerService.GetInteractionsAsync(Arg.Any<Guid>()).Returns(Task.FromResult<List<Models.Interaction>>(listOfInteractiones).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> RunFunction(string customerId)
+        {
+            return await GetInteractionHttpTrigger.Function.GetInteractionHttpTrigger.Run(
+                _request, _log, customerId, _resourceHelper, _getInteractionHttpTriggerService).ConfigureAwait(false);
+        }
+
+    }
+}
