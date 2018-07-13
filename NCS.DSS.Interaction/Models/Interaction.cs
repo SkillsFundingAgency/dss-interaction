ff863ea34083444af1c0c482e2a0af4a0f0d9f52
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-13 12:13:38 +0100

    Update Model

--- NCS.DSS.Interaction/Models/Interaction.cs
+++ NCS.DSS.Interaction/Models/Interaction.cs
@@ -24,12 +24,12 @@ namespace NCS.DSS.Interaction.Models
 
         [Display(Description = "Unique identifier of the adviser involved in the interaction.")]
         [Example(Description = "6eed4005-4364-4bcb-affb-170ee402d1aa")]
-        public Guid AdviserDetailsId { get; set; }
+        public Guid? AdviserDetailsId { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the interaction took place")]
         [Example(Description = "2018-06-21T11:21:00")]
-        public DateTime DateandTimeOfInteraction { get; set; }
+        public DateTime? DateandTimeOfInteraction { get; set; }
 
         [Display(Description = "Channel reference data")]
         [Example(Description = "1")]
@@ -42,11 +42,11 @@ namespace NCS.DSS.Interaction.Models
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time of the last modification to the record.")]
         [Example(Description = "2018-06-22T16:52:10")]
-        public DateTime LastModifiedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
         
         [Display(Description = "Identifier of the touchpoint who made the last change to the record")]
         [Example(Description = "d1307d77-af23-4cb4-b600-a60e04f8c3df")]
-        public Guid LastModifiedTouchpointId { get; set; }
+        public Guid? LastModifiedTouchpointId { get; set; }
 
         public void Patch(InteractionPatch interactionPatch)
         {
--- NCS.DSS.Interaction/Models/InteractionPatch.cs
+++ NCS.DSS.Interaction/Models/InteractionPatch.cs
@@ -9,16 +9,16 @@ namespace NCS.DSS.Interaction.Models
     {
         [Display(Description = "Unique identifier for the touchpoint with which the interaction took place.")]
         [Example(Description = "f823d23a-4006-4572-aef5-65ff085b4687")]
-        public Guid TouchpointId { get; set; }
+        public Guid? TouchpointId { get; set; }
 
         [Display(Description = "Unique identifier of the adviser involved in the interaction.")]
         [Example(Description = "6eed4005-4364-4bcb-affb-170ee402d1aa")]
-        public Guid AdviserDetailsId { get; set; }
+        public Guid? AdviserDetailsId { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time the interaction took place")]
         [Example(Description = "2018-06-21T11:21:00")]
-        public DateTime DateandTimeOfInteraction { get; set; }
+        public DateTime? DateandTimeOfInteraction { get; set; }
 
         [Display(Description = "Channel reference data")]
         [Example(Description = "1")]
@@ -31,11 +31,11 @@ namespace NCS.DSS.Interaction.Models
         [DataType(DataType.DateTime)]
         [Display(Description = "Date and time of the last modification to the record.")]
         [Example(Description = "2018-06-22T16:52:10")]
-        public DateTime LastModifiedDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
 
         [Display(Description = "Identifier of the touchpoint who made the last change to the record")]
         [Example(Description = "d1307d77-af23-4cb4-b600-a60e04f8c3df")]
-        public Guid LastModifiedTouchpointId { get; set; }
+        public Guid? LastModifiedTouchpointId { get; set; }
 
     }
 }
