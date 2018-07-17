3c24b7ff0c017d0d25c4c4d3a8407e4748185a79
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-17 15:59:08 +0100

    Add App Insights

---
+++ NCS.DSS.Interaction/AppInsights/ApplicationInsightsLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace NCS.DSS.Interaction.AppInsights
+{
+    public class ApplicationInsightsLoggerProvider : ILoggerProvider
+    {
+        private readonly Func<string, LogLevel, bool> _filter;
+        private readonly ApplicationInsightsSettings _settings;
+
+        public ApplicationInsightsLoggerProvider(Func<string, LogLevel, bool> filter, ApplicationInsightsSettings settings)
+        {
+            _filter = filter;
+            _settings = settings;
+        }
+
+        public ILogger CreateLogger(string name)
+        {
+            return new ApplicationInsightsLogger(name, _filter, _settings);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
