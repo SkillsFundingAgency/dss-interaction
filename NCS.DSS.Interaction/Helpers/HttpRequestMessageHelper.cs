96e1bb1dd89f547eae1ea8a5eb95139ac416b13a
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-08-01 14:52:28 +0100

    Set MediaType For Request

--- NCS.DSS.Interaction/Helpers/HttpRequestMessageHelper.cs
+++ NCS.DSS.Interaction/Helpers/HttpRequestMessageHelper.cs
@@ -9,6 +9,12 @@ namespace NCS.DSS.Interaction.Helpers
     {
         public async Task<T> GetInteractionFromRequest<T>(HttpRequestMessage req)
         {
+            if (req == null)
+                return default(T);
+
+            if (req.Content?.Headers?.ContentType != null)
+                req.Content.Headers.ContentType.MediaType = "application/json";
+
             return await req.Content.ReadAsAsync<T>();
         }
 
